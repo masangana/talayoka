60586be235312e1362c46332d6d4cdc02cd0bd8b
Alex Masangana <alexmasangana@gmail.com>
2023-11-22 16:03:28 +0300

    Start migration

--- database/migrations/2023_11_22_122312_create_user_profiles_table.php
+++ database/migrations/2023_11_22_122312_create_user_profiles_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
+            $table->string('image');
             $table->timestamps();
         });
     }
