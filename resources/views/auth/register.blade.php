598127dfdf6faedc24fb812d7b80c257cc1a4b19
Alex Masangana <alexmasangana@gmail.com>
2023-11-23 21:00:04 +0300

    Start home page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,79 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.user.app')
 
 @section('content')
 <div role="main" class="main shop py-4">
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <div class="container py-4">
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-5">
+                <h2 class="font-weight-bold text-5 mb-0">Inscription</h2>
+                <form action="{{ route('register') }}" id="frmSignUp" method="post">
+                    @csrf
+                    <div class="row">
+                        <div class="form-group col">
+                            <label class="form-label text-color-dark text-3">Nom d'Utilisateur<span class="text-color-danger">*</span></label>
+                            <input type="text" class="form-control form-control-lg text-4" 
+                                    id="name" name="name" value="{{ old('name') }}" 
+                                    required autocomplete="name" autofocus>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col">
+                            <label class="form-label text-color-dark text-3">Addresse E-mail <span class="text-color-danger">*</span></label>
+                            <input type="" class="form-control form-control-lg text-4"
+                            id="email" type="email" name="email" value="{{ old('email') }}" 
+                            required autocomplete="email">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col">
+                            <label class="form-label text-color-dark text-3">Mot de Passe <span class="text-color-danger">*</span></label>
+                            <input type="password" value="" class="form-control form-control-lg text-4"
+                                    id="password" type="password" name="password" value="{{ old('password') }}" 
+                                    required autocomplete="new-password">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col">
+                            <label class="form-label text-color-dark text-3">Confirmation Mot de Passe <span class="text-color-danger">*</span></label>
+                            <input type="password" value="" class="form-control form-control-lg text-4"
+                                    id="password-confirm" type="password" name="password_confirmation" value="{{ old('password') }}" 
+                                    required autocomplete="new-password">
                         </div>
-                        <input type="hidden" name="role" value="user">
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                    </div>
+                    <input type="hidden" id="role" name="role" value="user" />
+    
+                    <div class="row">
+                        <div class="form-group col">
+                            <p class="text-2 mb-2">
+                                Vos données personnelles seront utilisées pour soutenir votre expérience sur ce site Web,
+                                 pour gérer l'accès à votre compte, et à d'autres fins décrites dans notre
+                                 <a href="#" class="text-decoration-none">politique de confidentialité.</a>
+                            </p>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col">
+                            <button type="submit" class="btn btn-dark btn-modern w-100 text-uppercase rounded-0 font-weight-bold text-3 py-3" data-loading-text="Loading...">
+                                Inscription
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
+    
     </div>
-</div>
+
 </div>
 @endsection
