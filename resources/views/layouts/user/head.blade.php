566a0c5f7d19baefab6074c013da54aa8795c987
Alex Masangana <alexmasangana@gmail.com>
2023-11-23 15:30:36 +0300

    Add template assets

--- resources/views/layouts/user/head.blade.php
+++ resources/views/layouts/user/head.blade.php
@@ -21,27 +21,27 @@
     <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
 
     <!-- Vendor CSS -->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="vendor/animate/animate.compat.css">
-    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
-    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
-    <link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/animate/animate.compat.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/simple-line-icons/css/simple-line-icons.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/owl.carousel/assets/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/owl.carousel/assets/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/vendor/magnific-popup/magnific-popup.min.css')}}">
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" href="css/theme.css">
-    <link rel="stylesheet" href="css/theme-elements.css">
-    <link rel="stylesheet" href="css/theme-blog.css">
-    <link rel="stylesheet" href="css/theme-shop.css">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/theme.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/theme-elements.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/theme-blog.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/theme-shop.css')}}">
 
     <!-- Demo CSS -->
-    <link rel="stylesheet" href="css/demos/demo-creative-agency-2.css">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/demos/demo-creative-agency-2.css')}}">
 
     <!-- Skin CSS -->
-    <link id="skinCSS" rel="stylesheet" href="css/skins/skin-creative-agency-2.css">
+    <link id="skinCSS" rel="stylesheet" href="{{asset ('assets/user/css/skins/skin-creative-agency-2.css')}}">
 
     <!-- Theme Custom CSS -->
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="{{asset ('assets/user/css/custom.css')}}">
 
 </head>
